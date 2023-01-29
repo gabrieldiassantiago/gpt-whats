fe62a4c695cee476f192a48f595ad6485463fb90
Gabriel Dias <gabriel_dias_santiago@hotmail.com>
2023-01-29 00:19:57 -0300

    first commit

---
+++ src/bot.ts
@@ -0,0 +1,44 @@
+import { ChatGPT } from "./functions/chatgpt";
+import { connect } from "./core/connection";
+import { gTTS } from "./functions/gtts";
+
+export default async () => {
+    const socket = await connect();
+
+    socket.ev.on('messages.upsert', async (m) => {
+  
+
+        try{
+
+            const msg = m.messages[0].message.conversation || m.messages[0].message.extendedTextMessage.text;
+            const rJid = m.messages[0].key.remoteJid;
+
+            if (msg){
+                console.log(msg)
+                switch(msg.split(' ', 1)[0]){
+
+                    case '$gpt1':
+                        await socket.sendMessage(rJid, {text: 'Aguarde, vou gerar uma resposta para você'}, {quoted: m.messages[0]});
+                        await socket.sendMessage(rJid, {text: `${await ChatGPT(msg)}`}, {quoted: m.messages[0]});
+                        break;
+            
+                    
+
+                    case '$gpt-audio':
+                        await socket.sendMessage(rJid, {text: 'Gerando audio...'}, {quoted: m.messages[0]});
+                        await gTTS(msg).then(
+                            (audio) => {
+                                setTimeout(() => {
+                                    socket.sendMessage(rJid, {audio: {url: audio}, mimetype: 'audio/mp4', ptt: true}, {quoted: m.messages[0]})
+                                }, 1000)
+                            }
+                        );
+                        break;
+                        
+                };
+            };
+        }catch{
+            console.log('Esse tipo de mensagem não é suportada.');
+        };
+    });
+};
---
+++ src/functions/chatgpt.ts
@@ -0,0 +1,30 @@
+import { Configuration, OpenAIApi} from 'openai';
+require('dotenv').config();
+
+
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+});
+const openai = new OpenAIApi(configuration);
+
+
+
+export const ChatGPT = async (prompt: string) => {
+
+    let _prompt: string = prompt.replace('$gpt', '').replace('$gpt-tts', '');
+
+    const request = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: _prompt,
+        temperature: 0.2,
+        max_tokens: 1024,
+
+    });
+    console.log(request)
+
+    
+    const response: string = request.data.choices[0].text.replace('\n', '').replace('\n', '');
+
+    return response
+};
